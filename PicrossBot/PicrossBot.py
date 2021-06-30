1165ac3884cb6d8eb5320966467d9a7f65a3bce6
Jacqui <JSangster1@gmail.com>
2021-06-30 16:20:20 +0100

    Fill cells
    Added function to fill a given number of cells afrom a start cell in either a row or column

--- PicrossBot/PicrossBot.py
+++ PicrossBot/PicrossBot.py
@@ -3,6 +3,8 @@
 import pyautogui
 import math
 import numpy as np
+import time
+pyautogui.FAILSAFE = True
 
 class puzzleModel:
 
@@ -11,12 +13,20 @@ class puzzleModel:
 
     def parsePuzzle(self):
         print("called")
+        #click on window to ensure puzzle is in focus
+        pyautogui.click(10,10)
         #find the size of the grid
         #count the cells it finds
         cellLoc = list(pyautogui.locateAllOnScreen('cell.png', confidence = 0.95))
         #store the coord of first cell. this is 0,0.
         self.firstCell = pyautogui.center(cellLoc[0])
+        pyautogui.moveTo(self.firstCell)
+        time.sleep(1)
         self.cellSize = abs(self.firstCell[0] - pyautogui.center(cellLoc[1])[0])
+
+        #locate the toggle
+        self.togLoc = pyautogui.locateCenterOnScreen('toggle.png')
+        self.togFill = True
 
         #Maximum error is one either way
         if math.isqrt(len(cellLoc)) in range(4, 6):
@@ -43,7 +53,6 @@ class puzzleModel:
                         break
 
                 #same again for columns
-                pyautogui.moveTo(((self.firstCell[0] + (r * self.cellSize), self.firstCell[1] - (clue * self.cellSize))))
                 reg = (pyautogui.position()[0] - int(self.cellSize / 2), pyautogui.position()[1] - int(self.cellSize / 2), self.cellSize, self.cellSize)
                 
                 #check for each of the clues - there is probably a better way to do this.
@@ -58,50 +67,60 @@ class puzzleModel:
         #empty, marked, incorrect, correct
         self.grid = np.full((self.size, self.size), "empty")
 
-    def selectCell(self,row, col):
+    def selectCell(self,x, y):
         #work out what the xcoord and ycoord for that cell are
-        (xcoord, ycoord) = (int(self.firstCell[0] + (self.cellSize * col)), int(self.firstCell[1] + (self.cellSize * row)))
+        (xcoord, ycoord) = (int(self.firstCell[0] + (self.cellSize * x)), int(self.firstCell[1] + (self.cellSize * y)))
         pyautogui.click(xcoord, ycoord)
 
         #read what the cell changes to
         #given that the mouse should be in the center of the cell, we can use the colour of that pixel to decide
         #gray is marked, red is incorrect, black is correct
+        counter = 0
         checked = False
-        while checked == False:
+        print("checking")
+        while not(checked):
+            #add timer to prevent infinite loop
+            if counter > 10:
+                #change to error message
+                print("Stuck in loop. please close and retry.")
+                break
             #Due to an issue in pyautogui, repeat the pixel check until this identifies the new cell status
             try:
-                print(pyautogui.pixel(pyautogui.position()[0], pyautogui.position()[1]))
                 if pyautogui.pixelMatchesColor(xcoord,ycoord, (127, 127, 127), tolerance = 20):
-                    self.grid[row][col] = "marked"
-                    print("marked")
+                    self.grid[x][y] = "marked"
                     checked = True
                 elif pyautogui.pixelMatchesColor(xcoord, ycoord, (0, 0, 0), tolerance = 20):
-                    self.grid[row][col] = "correct"
-                    print("correct")
+                    self.grid[x][y] = "correct"
                     checked = True
                 elif pyautogui.pixelMatchesColor(xcoord, ycoord, (237, 28, 36), tolerance = 20):
-                    self.grid[row][col] = "incorrect"
-                    print("incorrect")
+                    self.grid[x][y] = "incorrect"
                     checked = True
                 else:
                     print("oops, something went wrong with detecting the cell")
+                    counter += 1
             except:
                 print("oops, something went wrong.")
+                counter += 1
 
     
     def toggleFill(self):
-        #work out where on the screen the toggle is
-        pyautogui.click(xcoord, ycoord)
+        #Click the toggle button
+        pyautogui.click(self.togLoc)
+        self.togFill = not(self.togFill)
+        
 
     def fillCells(self,fillNum, startX, startY, rowBool, fillBool):
         #check what mode the toggle is, and that it is what it needs to be
+        if (self.togFill != fillBool):
+            self.toggleFill(self)        
         for x in range(fillNum):
             if rowBool:
-                selectCell(startX + x, startY)
+                self.selectCell(self, startX + x, startY)
             else:
-                selectCell(startX, startY + x)
+                self.selectCell(self, startX, startY + x)
+        
 
-    #def markFullRow(row, col, rowBool):
+    #def markFullRow(x, y, rowBool):
         #check toggle is on tright mode
         #for length of whole grid
         #if rowBool
@@ -111,10 +130,6 @@ class puzzleModel:
 
 def main():
     puzzleModel.parsePuzzle(puzzleModel)
-    puzzleModel.selectCell(puzzleModel,0,0)
-    puzzleModel.selectCell(puzzleModel,4,4)
-    puzzleModel.selectCell(puzzleModel,2,4)
-    #puzzleModel.selectCell(puzzleModel,1,1)
-
+    
 if __name__ == "__main__":
     main()
